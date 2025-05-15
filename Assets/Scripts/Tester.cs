99dcc855a138663dd9df856e5007fab65cc18f2c
tt4keru <tt4keru@gmail.com>
2025-05-16 07:16:00 +0900

    Add Project

---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+	/// <summary>
+	/// Action
+	/// </summary>
+	public Action action;
+
+	/// <summary>
+	/// event Action
+	/// </summary>
+	public event Action eventAction;
+
+	/// <summary>
+	/// Actionに登録
+	/// </summary>
+	public void SubscribeAction()
+	{
+		Debug.Log("Actionに登録します。");
+		action += _OnActionCalled;
+	}
+
+	/// <summary>
+	/// event Actionに登録
+	/// </summary>
+	public void SubscribeEventAction()
+	{
+		Debug.Log("event Actionに登録します。");
+		eventAction += _OnEventActionCalled;
+	}
+
+	/// <summary>
+	/// 呼び出し
+	/// </summary>
+	public void Fire()
+	{
+		action?.Invoke();
+		eventAction?.Invoke();
+	}
+
+	/// <summary>
+	/// Actionをクリア
+	/// </summary>
+	public void Clear()
+	{
+		Debug.Log("Actionをクリアします。");
+		action = null;
+	}
+
+	/// <summary>
+	/// event Actionをクリア
+	/// </summary>
+	public void ClearEventAction()
+	{
+		Debug.Log("event Actionをクリアします。");
+
+		if (eventAction == null) {
+			return;
+		}
+
+		// GetInvocationList()で登録されたデリゲート一覧を取得
+		foreach (Delegate d in eventAction.GetInvocationList())
+		{
+			eventAction -= (Action)d;
+		}
+	}
+
+	/// <summary>
+	/// Actionが呼び出された時の処理
+	/// </summary>
+	private void _OnActionCalled()
+	{
+		Debug.Log("Actionが呼び出されました。");
+	}
+
+	/// <summary>
+	/// event Actionが呼び出された時の処理
+	/// </summary>
+	private void _OnEventActionCalled()
+	{
+		Debug.Log("event Actionが呼び出されました。");
+	}
+}
---
+++ Assets/Scripts/TesterUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TesterUI : MonoBehaviour
+{
+	/// <summary>
+	/// テスター
+	/// </summary>
+	[SerializeField]
+	private Tester _tester;
+
+	/// <summary>
+	/// ボタン
+	/// </summary>
+	[SerializeField]
+	private Button _subscribe_action_button;
+
+	/// <summary>
+	/// ボタン
+	/// </summary>
+	[SerializeField]
+	private Button _subscribe_event_action_button;
+
+	/// <summary>
+	/// ボタン
+	/// </summary>
+	[SerializeField]
+	private Button _fire_button;
+
+	/// <summary>
+	/// ボタン
+	/// </summary>
+	[SerializeField]
+	private Button _clear_action_button;
+
+	/// <summary>
+	/// ボタン
+	/// </summary>
+	[SerializeField]
+	private Button _clear_event_action_button;
+	
+	/// <summary>
+	/// 開始処理
+	/// </summary>
+	private void Start()
+	{
+		_subscribe_action_button.onClick.AddListener(_tester.SubscribeAction);
+		_subscribe_event_action_button.onClick.AddListener(_tester.SubscribeEventAction);
+		_fire_button.onClick.AddListener(_tester.Fire);
+		_clear_action_button.onClick.AddListener(_tester.Clear);
+		_clear_event_action_button.onClick.AddListener(_tester.ClearEventAction);
+	}
+}
